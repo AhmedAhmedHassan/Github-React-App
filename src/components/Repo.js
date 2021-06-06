8c3b9f50bf229d5b6f7109481a26c3722895eb85
AhmedAhmedHassan <ahmed.hassan@aru.edu.eg>
2021-06-06 03:38:47 -0700

    third commit

--- src/components/Repo.js
+++ src/components/Repo.js
@@ -8,7 +8,12 @@ const Repo =()=>{
     const handleClick = async()=>{
         try{
             const data= await axios('https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc')
-            const listRepos=data.data.items.map((item) => <li>{item.name}{item.stargazers_count}<img key={item.id} src={item.owner.avatar_url} /></li>)
+            const listRepos=data.data.items.map((item) => 
+            <li key={item.id}>
+                {item.name}
+                {item.stargazers_count}
+                <img  src={item.owner.avatar_url} />
+                </li>)
             // const listReposStar=data.data.items.map((item) => <li></li>)
             // const listReposImg=data.data.items.map((item) => <li> </li>)
             setRepos(listRepos)
