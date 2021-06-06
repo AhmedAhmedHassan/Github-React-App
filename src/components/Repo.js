e9248731b53a2cfd9455b04e06456627112322e4
AhmedAhmedHassan <ahmed.hassan@aru.edu.eg>
2021-06-06 03:36:41 -0700

    second comment

---
+++ src/components/Repo.js
@@ -0,0 +1,32 @@
+import React,{useState} from 'react'
+import axios from 'axios'
+
+const Repo =()=>{
+
+    const [repos,setRepos]=useState()
+   
+    const handleClick = async()=>{
+        try{
+            const data= await axios('https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc')
+            const listRepos=data.data.items.map((item) => <li>{item.name}{item.stargazers_count}<img key={item.id} src={item.owner.avatar_url} /></li>)
+            // const listReposStar=data.data.items.map((item) => <li></li>)
+            // const listReposImg=data.data.items.map((item) => <li> </li>)
+            setRepos(listRepos)
+            
+            // console.log(data.data.items[0].name)
+            
+        }catch(err){
+           console.log(err)
+        }
+
+    }
+    return(
+        <div>
+        <h1>hi</h1>
+         <li>{repos}</li>
+
+        <button onClick={handleClick}>Pull data</button>
+        </div>
+    )
+}
+export default Repo
